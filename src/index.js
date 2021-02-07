77ac1144a602bb120dc099306df1f52d0177ffb1
Giovanni Aranda <giovanni.arandaa@gmail.com>
2021-02-07 09:49:19 -0600

    Fundamentos

--- src/index.js
+++ src/index.js
@@ -8,9 +8,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges!</h1>;
-
+//const element = <h1>Hello, Platzi Badges!</h1>;
+//const element = React.createElement('a', {href: 'https://platzi.com'}, 'Hola! soy los children')
+const name = 'Richard'
+// const element = React.createElement(
+//   'h1',
+//   {},
+//   `Hola, soy ${name}`
+// )
+const jsx = <h1>Hola soy, {name}</h1>
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(jsx, container);
